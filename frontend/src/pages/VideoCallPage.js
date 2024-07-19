36b41f5050eb5b9e392cdbd75ed7ad2c4f36d161
keit0018 <bmomamnyi@gmail.com>
2024-07-19 16:15:34 +0300

    cleanup

--- frontend/src/pages/VideoCallPage.js
+++ frontend/src/pages/VideoCallPage.js
@@ -2,21 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import VideoCall from '../components/Videocall';
-import Chat from '../components/Chat';
 import '../assets/styles/VideoCallPage.css';
 
 const VideoCallPage = () => {
   const { meetingId } = useParams(); // Extracting meetingId from the URL
   const [participants, setParticipants] = useState([]);
-  const [isChatOpen, setIsChatOpen] = useState(false);
 
-  const toggleChat = () => {
-    setIsChatOpen(!isChatOpen);
-  };
 
   return (
     <div className="video-call-page">
-      <VideoCall meetingId={meetingId} onChatToggle={toggleChat} />
+      <VideoCall meetingId={meetingId}/>
     </div>
   );
 };
